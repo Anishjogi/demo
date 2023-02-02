486efcb51172e552f4f249882ff8c0753a8f93f3
Anishjogi <123926254+Anishjogi@users.noreply.github.com>
2023-02-02 10:56:15 +0530

    hii

---
+++ simple/SimpleInterest.java
@@ -0,0 +1,12 @@
+package simple;
+
+class SimpleInterest{
+    public static void main(String[] args) {
+        int p=100;
+        double t=2.9;
+        double r=30.3;
+        double si;
+        si=p*t*r/100;
+        System.out.println(si);
+    }
+}
