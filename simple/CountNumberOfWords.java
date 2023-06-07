b224f3c3d0614afe4effeee658ccc92203c8952a
Prasad <prasada@ttechlab.com>
2023-06-07 12:46:38 +0530

    "prasad added CountNumberOf Words class"

---
+++ simple/CountNumberOfWords.java
@@ -0,0 +1,12 @@
+package simple;
+
+import java.util.Scanner;
+
+public class CountNumberOfWords {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	System.out.println("please enter the words");
+	int n=scan.nextInt();
+	
+}
+}
