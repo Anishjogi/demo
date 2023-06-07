82c6cde81fc280700c6aab3a5d00af28d0997a6f
anishjogi172 <anishjogi7353012548@gmail.com>
2023-06-07 12:33:05 +0530

    resolved

--- simple/SimpleInterest.java
+++ simple/SimpleInterest.java
@@ -9,14 +9,14 @@ class SimpleInterest {
         si = p * t * r / 100;
         System.out.println(si); // helo
         System.out.println("hello anish");
-<<<<<<< HEAD
+
         System.out.println("main branch");
         System.out.println("main barnch 2");
         System.out.println("main 3");
-=======
+
         System.out.println("now i am in master branch");
         System.out.println("master 2");
->>>>>>> master
-    //master
+
+        // master
     }
 }
