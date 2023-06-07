c1a2c8d0373f56cbf485e7930ed70f03e28cd3f4
anishjogi172 <anishjogi7353012548@gmail.com>
2023-06-07 11:34:37 +0530

    added one print statement

--- simple/SimpleInterest.java
+++ simple/SimpleInterest.java
@@ -1,12 +1,13 @@
 package simple;
 
-class SimpleInterest{
+class SimpleInterest {
     public static void main(String[] args) {
-        int p=100;
-        double t=2.9;
-        double r=30.3;
+        int p = 100;
+        double t = 2.9;
+        double r = 30.3;
         double si;
-        si=p*t*r/100;
-        System.out.println(si); //helo
+        si = p * t * r / 100;
+        System.out.println(si); // helo
+        System.out.println("hello anish");
     }
 }
